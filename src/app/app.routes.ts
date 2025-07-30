39b8b79715c32465739cfebffe9128350c1677b2
adrianurbano30 <javoshine_adrian@hotmail.com>
2025-07-30 15:53:21 -0500

    actualizacion nueva pagina dragon ball uso de input basico uso de signals @for @if implementacion basica de interface

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,9 @@ import { Home } from './pages/home/home';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {path:'inicio', loadComponent: ()=>import('./pages/home/home').then(m => m.Home)},
+  {path:'', loadComponent: ()=>import('./pages/home/home').then(m => m.Home)},
   {path:'counter',loadComponent:()=>import('./pages/counter/counter').then(m=>m.Counter)},
   {path:'hero',loadComponent:()=>import('./pages/hero/hero').then(m=>m.Hero)},
-  {path:'**',redirectTo:'inicio'}
+  {path:'dragonball',loadComponent:()=>import('./pages/dragonball/dragonball').then(m=>m.Dragonball)},
+  {path:'**',redirectTo:''}
 ];
---
+++ src/app/pages/dragonball/dragonball.ts
@@ -0,0 +1,48 @@
+import { NgClass } from '@angular/common';
+import { Component, signal } from '@angular/core';
+
+interface chacracter{
+  id:number;
+  name:string;
+  power: number;
+}
+
+@Component({
+  selector: 'app-dragonball',
+  imports: [NgClass],
+  templateUrl: './dragonball.html',
+  styleUrl: './dragonball.css'
+})
+export class Dragonball {
+
+  name = signal('gohan');
+  power = signal(0);
+
+  characters = signal<chacracter[]>([
+    { id:1, name: 'Goku', power: 9000 },
+    { id:2, name: 'Vegeta', power: 8500 },
+    { id:3,name: 'Gohan', power: 8000 },
+    { id:4, name: 'Piccolo', power: 7000 },
+    { id:5, name: 'Frieza', power: 10000 },
+    { id:6, name: 'Cell', power: 9500 },
+    { id:7, name: 'Majin Buu', power: 12000 }
+  ]);
+
+  addcharacter(){
+
+    if (!this.name() || !this.power || this.power()<=0) {
+      return;
+    }
+    const newcharacter:chacracter = {
+      id:this.characters().length + 1,
+      name:this.name(),
+      power:this.power()
+    }
+    //this.characters().push(newcharacter);
+    this.characters.update((lista)=>[...lista,newcharacter] )
+
+    this.name.set('');
+    this.power.set(0);
+  }
+
+}
