975de706f12dcd2a142ab1a9f1a00b087f809701
adrianurbano30 <javoshine_adrian@hotmail.com>
2025-07-31 14:36:23 -0500

    alteracion de la configuracion de la app para que en la caja de rutas no haya problemas USO DE LOCATIONSTRATEGY

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
   ]
 };
